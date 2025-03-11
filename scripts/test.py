7dc83b95efa9b1bb0db22276614f0fc4322c6dde
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-03-11 20:09:19 +0900

    modified test

--- scripts/test.py
+++ scripts/test.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
 
     conn = sqlite3.connect(f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/segment_details.db")
 
-    query_all = f"SELECT * FROM segment_details_data WHERE tour_year = '{grand_tour}-{year}'"
+    segment_query = f"SELECT * FROM segment_details_data WHERE tour_year = '{grand_tour}-{year}'"
 
-    df = pd.read_sql_query(query_all, conn)
+    df = pd.read_sql_query(segment_query, conn)
     activity_ids = set(df["stage"])
     nonoverlapping_segments = pd.DataFrame()
     for id in activity_ids:
@@ -28,3 +28,12 @@ if __name__ == "__main__":
         nonoverlapping_segments = pd.concat(
             [nonoverlapping_segments, segment_analyse.name_getter(stage_df, max_coverage)]
         )
+
+    conn = sqlite3.connect(f"/Users/{username}/iCloud/Research/Data_Science/Projects/data/grand_tours.db")
+
+    strava_query = (
+        f"SELECT activity_id, athlete_id, strava_distance, stage, segment "
+        f"FROM strava_table WHERE tour_year = '{grand_tour}-{year}'"
+    )
+    df = pd.read_sql_query(strava_query, conn)
+    print(df)

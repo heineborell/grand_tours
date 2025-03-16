392ff099137e3f5e306c475dcf012d6e23366551
Deniz Deveci <dodeve@gmail.com>
2025-03-17 00:11:14 +0900

    working get_rider func

--- scripts/fetching.py
+++ scripts/fetching.py
@@ -1,7 +1,9 @@
-from data.database import fetch_rides
+from data.database import get_rider
 
 DB_PATH_1 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/training.db"
 DB_PATH_2 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/grand_tours.db"
 
-activities = fetch_rides(db_path_activity=DB_PATH_1, db_path_name=DB_PATH_2)
-print(activities)
+# activities = fetch_rides(db_path_activity=DB_PATH_1, db_path_name=DB_PATH_2)
+
+rider = get_rider(133606, "stats_data", DB_PATH_2, DB_PATH_1)
+print(rider)

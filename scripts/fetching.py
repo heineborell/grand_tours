6282e5cb35d7bff21ee99330952acd5e52abae6f
Deniz Deveci <dodeve@gmail.com>
2025-03-16 22:10:20 +0900

    combined training,names

--- scripts/fetching.py
+++ scripts/fetching.py
@@ -1,4 +1,7 @@
 from data.database import fetch_rides
 
-DB_PATH = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/training.db"
-activities = fetch_rides(DB_PATH=DB_PATH)
+DB_PATH_1 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/training.db"
+DB_PATH_2 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/grand_tours.db"
+
+activities = fetch_rides(db_path_activity=DB_PATH_1, db_path_name=DB_PATH_2)
+print(activities)
--- src/data/database.py
+++ src/data/database.py
@@ -1,40 +1,31 @@
+import os
 import sqlite3
 
 # from data.schemas import Rider, Ride
 
 
-def get_db_connection(DB_PATH):
-    return sqlite3.connect(DB_PATH)
+def fetch_rides(db_path_activity, db_path_name):
+    # Connect to an in-memory database
+    conn_memory = sqlite3.connect(":memory:")
+    cursor = conn_memory.cursor()
 
+    # Convert to absolute paths
+    db_path_activity = os.path.abspath(db_path_activity)
+    db_path_name = os.path.abspath(db_path_name)
 
-def fetch_rides(DB_PATH):
-    """Fetch all rides given date from the database."""
-    conn = get_db_connection(DB_PATH)
-    cursor = conn.cursor()
+    # Attach databases safely
+    cursor.execute("ATTACH DATABASE ? AS db1", (db_path_activity,))
+    cursor.execute("ATTACH DATABASE ? AS db2", (db_path_name,))
 
-    cursor.execute("SELECT * FROM stats_data")
+    # Create a query that joins data from both databases
+    query = """
+    SELECT *
+    FROM db1.stats_data t1
+    INNER JOIN db2.strava_names t2 ON t1.athlete_id = t2.athlete_id
+    """
+
+    # Fetch directly with SQLite
+    cursor.execute(query)
     riders_data = cursor.fetchall()
+
     return riders_data
-
-
-# # Connect to an in-memory database
-# conn_memory = sqlite3.connect(":memory:")
-# cursor = conn_memory.cursor()
-#
-# # Attach both databases
-# cursor.execute("ATTACH DATABASE 'database1.db' AS db1")
-# cursor.execute("ATTACH DATABASE 'database2.db' AS db2")
-#
-# # Create a query that joins data from both databases
-# query = """
-# SELECT *
-# FROM db1.table_name1 t1
-# INNER JOIN db2.table_name2 t2 ON t1.common_column = t2.common_column
-# """
-#
-#
-# # Fetch directly with SQLite
-# cursor.execute(query)
-# merged_data = cursor.fetchall()
-#
-# conn_memory.close()

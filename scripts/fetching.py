8b44f9fd3de5aac5e0be69bd9eef52591006a63c
Deniz Deveci <dodeve@gmail.com>
2025-03-17 11:37:06 +0900

    changed the db to merged training

--- scripts/fetching.py
+++ scripts/fetching.py
@@ -1,9 +1,9 @@
 from data.database import get_rider
 
-DB_PATH_1 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/training.db"
+DB_PATH_1 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/training_merged.db"
 DB_PATH_2 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/grand_tours.db"
 
 # activities = fetch_rides(db_path_activity=DB_PATH_1, db_path_name=DB_PATH_2)
 
-rider = get_rider(4018952, "stats_data", DB_PATH_2, DB_PATH_1)
+rider = get_rider(4018952, "training_table", DB_PATH_2, DB_PATH_1)
 print(rider)

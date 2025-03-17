4e116603947162ba814f6e785f45145cc77f163a
Deniz Deveci <dodeve@gmail.com>
2025-03-17 10:04:00 +0900

    fixed avg power

--- src/data/schema.py
+++ src/data/schema.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
@@ -8,7 +8,8 @@ class Ride(BaseModel):
     distance: float = Field(..., gt=0, description="Ride in kilometers.")
     time: float = Field(..., gt=0, description="Time in hours:minutes.")
     elevation: int = Field(..., gt=0, description="Total ascension in meters.")
-    # avg_power: Optional[int] = None
+    avg_power: Optional[int] = None
+    tour_year: str = Field(..., description="Tour year and shows training or not")
 
 
 class Rider(BaseModel):

737d47c6d722442fe588b20ff7973109046d3659
Deniz Deveci <dodeve@gmail.com>
2025-03-17 14:42:33 +0900

    fixed avg power

--- scripts/fetching.py
+++ scripts/fetching.py
@@ -5,6 +5,7 @@ DB_PATH_2 = "/Users/deniz/iCloud/Research/Data_Science/Projects/data/grand_tours
 
 # activities = fetch_rides(db_path_activity=DB_PATH_1, db_path_name=DB_PATH_2)
 
-rider = get_rider(189040, "training_table", DB_PATH_2, DB_PATH_1)
+rider = get_rider(10045754, "training_table", DB_PATH_2, DB_PATH_1)
+# print(rider.model_dump())
 for ride in rider.rides:
-    print(ride.distance)
+    print(ride.avg_power)

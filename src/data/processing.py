cba2ec59fd8d0cb619b3738ba1cabcc63e794b4c
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-03-23 22:28:04 +0900

    started individual rider training

--- src/data/processing.py
+++ src/data/processing.py
@@ -32,6 +32,8 @@ def create_dataframe(rider_ids, tour, year, db_path, training=True):
         if rider:  # Ensure rider is not None
             dfs.append(rider.to_dataframe())
 
+    # here if a dataframe is all NaN they are dropped or if an entry is None its
+    # changed into NaN.
     dfs = [df for df in dfs if not df.empty if not df.empty and not df.isna().all().all()]
     dfs = [df.map(lambda x: np.nan if (x is pd.NA or x is None) else x) for df in dfs]
 
@@ -44,8 +46,6 @@ def create_dataframe(rider_ids, tour, year, db_path, training=True):
 
 def clean_dataframe(data):
     data = data[(data["distance"] > 0) & (data["elevation"] > 0)].dropna(subset=["time"])
-    if "avg_power" in data.columns:
-        data = data.dropna(subset=["avg_power"])
     return data
 
 
@@ -55,32 +55,3 @@ def create_features(data, training):
     if training:
         data = data.drop(index=data.loc[data["ride_day"] > data["race_start_day"]].index)
     return data
-
-
-# ## make empty rmse holder
-# cv_rmses = np.zeros((5, len(models)))
-#
-#
-# def kfold_split_train(X_train, models):
-#     kfold = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
-#     ## loop through all splits
-#     for train_index, test_index in kfold.split(X_train):
-#         ## get train and holdout sets
-#         X_train_train = X_train.iloc[train_index]
-#         X_holdout = X_train.iloc[test_index]
-
-
-#
-#         ## loop through all models
-#         j = 0
-#         for model in models:
-#                 ## make clone
-#                 reg = SVR(**best_params_SVR)
-#
-#                 ## fit clone
-#                 reg.fit(chl_train_train[model], chl_train_train.chl)
-#                 predict = reg.predict(chl_holdout[model])
-#
-#                 ## record mse
-#                 cv_rmses[i, j] = root_mean_squared_error(chl_holdout.chl, predict)
-#             j = j + 1

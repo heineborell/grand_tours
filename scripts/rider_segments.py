693b3b2dcdd5ac5a4b9d855b8f65edf2d0472f31
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-04-11 17:26:01 +0900

    working training

--- scripts/rider_segments.py
+++ scripts/rider_segments.py
@@ -1,15 +1,18 @@
+import json
 from pathlib import Path
 
 from rich import print
+from sklearn.model_selection import KFold, train_test_split
 
 from data.data_loader import config_loader
 from data.database import get_rider
 from data.processing import fetch_riders
+from training.kfold import kfold_split_train
 
 if __name__ == "__main__":
     # Get the absolute path of the project root dynamically
     PROJECT_ROOT = Path(__file__).resolve().parent.parent  # Adjust if needed
-    CONFIG_PATH = PROJECT_ROOT / "config/config.json"
+    CONFIG_PATH = PROJECT_ROOT / "config/config_segments.json"
     DB_PATH = PROJECT_ROOT / "config/db_path.json"
 
     # Enter tour, year of your choice
@@ -28,4 +31,18 @@ if __name__ == "__main__":
     #         if rider.to_segment_df() is not None:
     #             print(f"rider id:{id}", len(rider.to_segment_df()))
     rider = get_rider(1905161, tour, year, DB_PATH, training=True, segment_data=True)
-    print(rider.to_segment_df())
+    data = rider.to_segment_df()
+    print(data[["distance", "vertical", "grade", "time"]].isnull().sum())
+
+    # # Splitting train and test
+    X_train, X_test = train_test_split(data, test_size=0.2, random_state=42)
+
+    # params = param_search(X_train=X_train, config_path=config_path)
+    # json_writer(config_path, params)
+
+    with open(config_path, "r") as f:
+        config = json.loads(f.read())
+
+    kfold = KFold(n_splits=5, shuffle=True, random_state=31)
+
+    kfold_split_train(X_train, config, kfold)

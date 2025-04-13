2cc6d0168716d8e9ba0c2821d7b61e8619ab8d67
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-04-13 23:29:04 +0900

    added kfold_train segment per rider

--- scripts/segment_kfold_train.py
+++ scripts/segment_kfold_train.py
@@ -1,13 +1,11 @@
+import json
 from pathlib import Path
 
-from rich import print
-from sklearn.model_selection import train_test_split
-from xgboost import training
+from rich import print as print
+from sklearn.model_selection import KFold, train_test_split
 
-from data.data_loader import config_loader
-from data.database import get_rider
-from data.processing import create_features, fetch_riders
-from training.gridsearch import json_writer, param_search
+from data.processing import create_features, get_rider
+from training.kfold import kfold_split_train
 
 if __name__ == "__main__":
     # Get the absolute path of the project root dynamically
@@ -18,17 +16,12 @@ if __name__ == "__main__":
     # Enter tour and set if it is training or race, here we merged all years together
     tour = "tdf"
     training = True
-    years = [2023]
-
-    # Open base config and prepare it for parameter search by entering the tour and years you selected
-    config_path = Path(config_loader(tour, years, config_path=CONFIG_PATH))
-
-    # Get rider list
-    rider_list = fetch_riders(DB_PATH, tour, years[0])
-    total_riders = len(rider_list)
-    print(f"The number of riders is {total_riders}.")
+    years = [2024]
 
     for year in years:
+        with open(PROJECT_ROOT / f"config/config_{tour}_training-{year}_individual.json", "r") as f:
+            train_config = json.loads(f.read())
+        rider_list = [conf["strava_id"] for conf in train_config]
         for i, id in enumerate(list(rider_list)):
             try:
                 rider = get_rider(id, tour, year, DB_PATH, training=True, segment_data=True)
@@ -39,15 +32,18 @@ if __name__ == "__main__":
                             f"Number of segments {len(rider.to_segment_df())}.",
                         )
                         data = rider.to_segment_df()
-                        print(data[["distance", "vertical", "grade", "time"]].isnull().sum())
                         create_features(data, training=True)
 
                         # # Splitting train and test
                         X_train, X_test = train_test_split(data, test_size=0.2, random_state=42)
+                        config = next((item for item in train_config if item["strava_id"] == id), None)
 
-                        params = param_search(X_train=X_train, config_path=config_path)
-                        json_writer(config_path, params, id)
-                    else:
-                        print(f"[bold pink] The rider {id} has no training rides. [/bold pink]")
+                        kfold = KFold(n_splits=5, shuffle=True, random_state=31)
+                        kfold_split_train(X_train, config, kfold)
+            #
+            #                     params = param_search(X_train=X_train, config_path=config_path)
+            #                     json_writer(config_path, params, id)
+            #                 else:
+            #                     print(f"[bold pink] The rider {id} has no training rides. [/bold pink]")
             except Exception as e:
                 print(f"Error processing rider {id}: {e}")

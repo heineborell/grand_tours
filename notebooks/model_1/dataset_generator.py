b177371c9bcec479a700e5417452f22a3747a28a
nedgren <nealedgren@icloud.com>
2025-04-23 20:37:12 -0500

    Fix filepath issues

--- notebooks/model_1/dataset_generator.py
+++ notebooks/model_1/dataset_generator.py
@@ -1,13 +1,28 @@
+'''This script uses the function in data_compiler to generate dataframes for 2020-2023 and 2024,
+and puts them in csv files in the data folder for model 1.'''
+
 import data_compiler
 from sklearn.model_selection import train_test_split
+from pathlib import Path
 
 data_2020_2023 = data_compiler.generate_race_data_with_training_features(range(2020,2024))
 data_2024 = data_compiler.generate_race_data_with_training_features(2024)
 
 data_train, data_test = train_test_split(data_2020_2023, train_size=0.8, random_state=42, shuffle=True)
 
-data_2020_2023.to_csv(r'data/2020-2023-combined-race-training-data.csv', index=False)
-data_2024.to_csv(r'data/2024-combined-race-training-data.csv', index=False)
+#Get current directory
+current_directory = Path(__file__).resolve().parent
+DATA_PATH = current_directory / 'data'
 
-data_train.to_csv('data/model-training-set.csv', index=False)
-data_test.to_csv('data/model-testing-set.csv', index=False)
+data_2020_2023_path = DATA_PATH / '2020-2023-combined-race-training-data.csv'
+data_2024_path = DATA_PATH / '2024-combined-race-training-data.csv'
+training_path = DATA_PATH / 'model-training-set.csv'
+test_path = DATA_PATH / 'model-testing-set.csv'
+
+data_2020_2023.to_csv(data_2020_2023_path, index=False)
+data_2024.to_csv(data_2024_path, index=False)
+
+data_train.to_csv(training_path, index=False)
+data_test.to_csv(test_path, index=False)
+
+print("Datasets generated successfully!")
